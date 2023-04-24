5cfc72edf41ee753e8fe46175721696919de3614
Nevindu7246 <nevinduheshan4@gmail.com>
2023-04-24 15:01:07 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+//import link
+import { Link } from "react-router-dom";
+
+//import logo
+import Logo from "../assets/img/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="py-6 mb-12 border-b">
+      <div className="container flex items-center justify-between mx-auto">
+        {/* logo */}
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
+        {/* button */}
+        <div className="flex items-center gap-6">
+          <Link className="transition hover:text-violet-900" to="">Log In</Link>
+          <Link className="px-4 py-3 text-white transition rounded-lg bg-violet-700 hover:bg-violet-800" to="">Sign Up</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
